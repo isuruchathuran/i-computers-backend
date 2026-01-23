aa653784283e249e018a0674507e4357a269ca6e
isuruchathuran <isuruchathuran24@gmail.com>
2026-01-23 20:12:14 +0530

    Day 16 - setup database connection (Frontend) 🚀

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,101 +1,92 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export function createUser(req, res) {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
 
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  const user = new User({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: hashedPassword,
+    image: req.body.image,
+  });
 
-    const user = new User({
-        email: req.body.email,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        password: hashedPassword,
-        image : req.body.image
+  user
+    .save()
+    .then(() => {
+      res.json({
+        message: "User created Successfully...",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "user creation Failed...",
+      });
+    });
+}
+
+export async function loginUser(req, res) {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
     });
 
-    user.save().then(
-        () => {
-            res.json({
-                message: "User created Successfully..."
-            });
-        }
-    ).catch(
-        () => {
-            res.json({
-                message: "user creation Failed..."
-            });
-        }
+    if (!user) {
+      return res.status(404).json({
+        message: "User with given email not found...",
+      });
+    }
+
+    const isPasswordValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
     );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Incorrect Password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+        image: user.image,
+        isEmailVerified: user.isEmailVerified,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    return res.json({
+      message: "Login Successful...",
+      token: token,
+      role: user.role  
+    });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "Internal server error...",
+    });
+  }
 }
 
-export function loginUser(req, res) {
-
-    User.findOne({
-        email: req.body.email
-    }).then( 
-        (user) => {
-
-            if (user == null) {
-                res.json({
-                    message: "User with given email not Found..."
-                });
-            } else {
-
-                const isPasswordValid = bcrypt.compareSync(
-                    req.body.password,
-                    user.password
-                );
-
-                if (isPasswordValid) {
-
-                    const token = jwt.sign({
-                        email : user.email,
-                        firstName : user.firstName,
-                        lastName : user.lastName,
-                        role : user.role,
-                        image : user.image,
-                        isEmailVerified : user.isEmailVerified
-                    } , "i-computers-54!")
-
-                    console.log(token)
-                    console.log({
-                        email : user.email,
-                        firstName : user.firstName,
-                        lastName : user.lastName,
-                        role : user.role,
-                        image : user.image,
-                        isEmailVerified : user.isEmailVerified
-                    });
-
-
-                    res.json({
-                        message: "Login Successful...",
-                        token : token,
-                    });
-
-                } else {
-                    res.status(401).json({
-                        message: "Login Failed..."
-                    });
-                }
-            }
-        }
-    ).catch(
-        ()=>{
-        res.status(500).res.json({
-            message : "Internal server error..."
-        })
-    })
-}
-
-export function isAdmin(req){
-    if(req.user == null){
-        return false
-    }
-    if(req.user.role == "admin"){
-        return true
-    }else{
-        return false
-    }
+export function isAdmin(req) {
+  if (req.user == null) {
+    return false;
+  }
+  if (req.user.role == "admin") {
+    return true;
+  } else {
+    return false;
+  }
 }
--- index.js
+++ index.js
@@ -3,29 +3,35 @@ import mongoose from "mongoose"
 import userRouter from './router/userRouter.js'
 import productRouter from './router/productRouter.js'
 import AuthorizeUser from './lib/jwtMiddleware.js'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
-const mongoURI = "mongodb+srv://admin:1234@cluster0.viide9k.mongodb.net/?appName=Cluster0"
+const mongoURI = process.env.MONGO_URL
 
-mongoose.connect(mongoURI).then(
-    ()=>{
-        console.log("Connected to MongoDB....")
-    } 
-).catch(
-    ()=>{
-        console.log("ErrorConnecting to MongoDB....")
-    }
-)
+async function connectMongoDB() {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB...");
+  } catch (error) {
+    console.error("Error Connecting to MongoDB....", error);
+  }
+}
+
+connectMongoDB();
 
 const app = express()
+
+app.use( cors() )
 
 app.use(express.json())
 
 app.use(AuthorizeUser)
 
 
-app.use("/users" ,userRouter)
-app.use("/products" ,productRouter )
+app.use("/api/users" ,userRouter)
+app.use("/api/products" ,productRouter )
    
 
 app.listen(3000,
@@ -33,4 +39,4 @@ app.listen(3000,
         console.log("server is running on port 3000")
     }
 )
-
+ 
